69cef032373f8ade0d063641cc87b9ec615c657f
sawmon <sywmon@gmail.com>
2014-06-13 10:37:43 +0630

    FIRSTCOMMIT
    commit frist time

---
+++ oopexam1for7batch(SawMon)/oopexam1/src/coffeeworld/Decaf.java
@@ -0,0 +1,24 @@
+package coffeeworld;
+
+public class Decaf extends Beverage{
+
+	@Override
+	public double cost() {
+		
+		return 1.05;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return super.getDescription();
+	}
+
+	@Override
+	public void setDescription(String description) {
+		// TODO Auto-generated method stub
+		super.setDescription(description);
+	}
+	
+
+}
